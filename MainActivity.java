e775e500578887ff024f53ccbd1e004968eb391c
TaRipoSio <se57160146@mgail.com>
2017-04-23 15:44:10 +0700

    services -profile -resume -position -insignia

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,48 @@
 package com.tariposio.team06;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.tariposio.team06.HRservices.HRservices;
+import com.tariposio.team06.HRservices.ProfileServices.ProfileServices;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HRservices service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://10.51.4.17/TSP57/PCK/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(HRservices.class);
+
+        service.getResumedata().enqueue(new Callback<ProfileServices>() {
+            @Override
+            public void onResponse(Call<ProfileServices> call, Response<ProfileServices> response) {
+                Log.d("Response","Success");
+                Log.d("Response",response.message());
+                Toast.makeText(MainActivity.this,
+                        "Your Message", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<ProfileServices> call, Throwable t) {
+                Log.d("Response","Fail");
+                Log.d("Response",t.getMessage());
+            }
+        });
     }
 }
