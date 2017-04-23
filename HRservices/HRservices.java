ae381fcbde8351dc9d6ac739dc7e1fe4eafb93a9
TaRipoSio <se57160146@mgail.com>
2017-04-23 18:59:31 +0700

    services -profile -resume -position -insignia

--- HRservices/HRservices.java
+++ HRservices/HRservices.java
@@ -8,13 +8,14 @@ import retrofit2.http.POST;
  */
 
 public interface HRservices {
+
     @POST("index.php/hr/official/get_profile_json/950")
     Call<ProfileServices> getProfileData();
 
     @POST("index.php/hr/official/get_resume_json/950")
     Call<ResumeServices> getResumeData();
 
-    @POST("index.php/hr/official/get_position_json/950")
+    @POST("index.php/hr/official/get_position_json")
     Call<PositionServices> getPositionData();
 
     @POST("index.php/hr/official/get_insignia_json/950")
--- MainActivity.java
+++ MainActivity.java
@@ -5,14 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.tariposio.team06.HRservices.HRservices;
-import com.tariposio.team06.HRservices.ProfileServices;
+import com.tariposio.team06.HRservices.PositionServices;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,25 +27,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Define the interceptor, add authentication headers
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder().addHeader("User-Agent", "Retrofit-Sample-App").build();
+                return chain.proceed(newRequest);
+            }
+        };
+
+        // Add the interceptor to OkHttpClient
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.interceptors().add(interceptor);
+        OkHttpClient client = builder.build();
 
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.51.4.17/TSP57/PCK/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         service = retrofit.create(HRservices.class);
 
-        service.getProfileData().enqueue(new Callback<ProfileServices>() {
+        service.getPositionData().enqueue(new Callback<PositionServices>() {
             @Override
-            public void onResponse(Call<ProfileServices> call, Response<ProfileServices> response) {
+            public void onResponse(Call<PositionServices> call, Response<PositionServices> response) {
                 Log.d("Response",response.message());
             }
 
             @Override
-            public void onFailure(Call<ProfileServices> call, Throwable t) {
+            public void onFailure(Call<PositionServices> call, Throwable t) {
                 Log.d("Response",t.getMessage());
             }
         });
+
     }
 }
